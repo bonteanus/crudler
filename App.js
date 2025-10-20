dd59de76fe4f43c06b7574628418c4c55e8bf566
bonteanus <sbonteanu04@gmail.com>
2025-10-20 16:36:41 +0100

    screens created

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import ModuleListScreen from "./src/components/screens/ModuleListScreen";
+import ModuleAddScreen from "./src/components/screens/ModuleAddScreen";
+import ModuleViewScreen from "./src/components/screens/ModuleViewScreen";
+import ModuleModifyScreen from "./src/components/screens/ModuleModifyScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -11,11 +14,33 @@ export const App = () => {
   // View -----------------------------
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="ModuleListScreen">
+      <Stack.Navigator
+        initialRouteName="ModuleAddScreen"
+        screenOptions={{
+          headerStyle: { backgroundColor: "black" },
+          headerTintColor: "white",
+        }}>
         <Stack.Screen
           name="ModuleListScreen"
           component={ModuleListScreen}
           options={{ title: "List modules" }}
+        />
+        <Stack.Screen
+          name="ModuleAddScreen"
+          component={ModuleListScreen}
+          options={{ title: "Add module" }}
+        />
+
+        <Stack.Screen
+          name="ModuleViewScreen"
+          component={ModuleListScreen}
+          options={{ title: "View module" }}
+        />
+
+        <Stack.Screen
+          name="ModuleModifyScreen"
+          component={ModuleListScreen}
+          options={{ title: "Modify module" }}
         />
       </Stack.Navigator>
     </NavigationContainer>
